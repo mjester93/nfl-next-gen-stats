1010036fecfae1e8fbbf59e3be0e72f569402a1a
Michael Jester <m.jester.93@gmail.com>
2020-11-14 21:08:07 -0500

    added player model

--- nfl_next_gen_stats/models.py
+++ nfl_next_gen_stats/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    full_name = models.CharField(max_length=200)
+    short_name = models.CharField(max_length=200)
+    gsis_id = models.CharField(max_length=20)
