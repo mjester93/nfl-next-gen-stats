4db500a12ccda4d66700ed2cc24063ea9a17bac9
Michael Jester <m.jester.93@gmail.com>
2020-11-14 21:22:58 -0500

    get passing data from github

---
+++ nfl_next_gen_stats/management/commands/import_stats.py
@@ -0,0 +1,27 @@
+from bulk_sync import bulk_sync
+from django.core.management.base import BaseCommand, CommandError
+from ...models import Player, PassingStats
+import pandas as pd
+import requests
+import time
+
+class Command(BaseCommand):
+    help = 'Imports NGS from the github data page'
+    
+    def handle(self, *args, **options):
+        years = [2016, 2017, 2018, 2019, 2020]
+
+        for year in years:
+            current_passing_data = pd.read_csv(
+                "https://github.com/mrcaseb/nfl-data/blob/master/data/ngs/ngs_" + year +"_passing.csv.gz?raw=True",
+                compression='gzip', 
+                low_memory=False,
+            )
+
+            final_passing_data = final_passing_data.append(current_passing_data, sort=True)
+            
+            # TODO ADD RUSHING AND RECEIVER URLS
+
+        #Give each row a unique index
+        final_passing_data.reset_index(drop=True, inplace=True)
+
