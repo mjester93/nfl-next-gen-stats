420594db3e36a068c95a9703d4f342be19d9ff42
Michael Jester <m.jester.93@gmail.com>
2020-11-16 13:03:52 -0500

    simple home page

--- nfl_next_gen_stats/urls.py
+++ nfl_next_gen_stats/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import include, path
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('player/<str:gsis_id>/', views.player_page, name='player-page'),
     path('player/<str:gsis_id>/<int:season>/', views.player_page, name='player-season')
 ]
--- nfl_next_gen_stats/views.py
+++ nfl_next_gen_stats/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import get_object_or_404, render
 from .models import *
 
 
+def home(request):
+    return render(request, 'nfl_next_gen_stats/home.html')
+
+
 def player_page(request, gsis_id=None, season=None):
 
     player = get_object_or_404(Player, gsis_id=gsis_id)
