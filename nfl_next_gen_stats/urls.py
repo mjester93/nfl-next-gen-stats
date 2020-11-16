a02da286e097e7b6eba573f70d45503b470a40b3
Michael Jester <m.jester.93@gmail.com>
2020-11-16 12:53:23 -0500

    showing season

--- nfl_next_gen_stats/urls.py
+++ nfl_next_gen_stats/urls.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('player/<str:gsis_id>/', views.player_page, name='player-page'),
+    path('player/<str:gsis_id>/<int:season>/', views.player_page, name='player-season')
 ]
--- nfl_next_gen_stats/views.py
+++ nfl_next_gen_stats/views.py
@@ -1,17 +1,34 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 from .models import *
 
 
-def player_page(request, gsis_id=None):
+def player_page(request, gsis_id=None, season=None):
 
     player = get_object_or_404(Player, gsis_id=gsis_id)
+    passing_stats = player.passing_stats().order_by('season', 'week')
+    rushing_stats = player.rushing_stats().order_by('season', 'week')
+    receiving_stats = player.receiving_stats().order_by('season', 'week')
 
     context = {
         'player': player,
-        'title': player.full_name,
-        'passing_stats': player.passing_stats().order_by('season', 'week'),
-        'rushing_stats': player.rushing_stats().order_by('season', 'week'),
-        'receiving_stats': player.receiving_stats().order_by('season', 'week')
     }
+
+    if season:
+        context.update({
+            'title': f"{player.full_name} -- {season}",
+            'season': season,
+            'passing_stats': passing_stats.filter(~Q(week=0), season=season),
+            'rushing_stats': rushing_stats.filter(~Q(week=0), season=season),
+            'receiving_stats': receiving_stats.filter(~Q(week=0), season=season),
+        })
+
+    else:
+        context.update({
+            'title': player.full_name,
+            'passing_stats': passing_stats.filter(week=0),
+            'rushing_stats': rushing_stats.filter(week=0),
+            'receiving_stats': receiving_stats.filter(week=0),
+        })
 
     return render(request, 'nfl_next_gen_stats/player.html', context)
