72c2e22161b143be1a9cd42b37a48d52634c6171
Michael Jester <m.jester.93@gmail.com>
2020-11-30 12:17:20 -0500

    adding position field

--- nfl_next_gen_stats/models.py
+++ nfl_next_gen_stats/models.py
@@ -8,6 +8,7 @@ class Player(models.Model):
     short_name = models.CharField(max_length=200)
     gsis_id = models.CharField(max_length=20, unique=True)
     team = models.CharField(max_length=20, blank=True, null=True)
+    position = models.CharField(max_length=20, blank=True, null=True)
 
     def passing_stats(self):
         return PassingStats.objects.filter(gsis_id=self.gsis_id)
